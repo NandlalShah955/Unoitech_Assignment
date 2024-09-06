bf869d010178f42166ccbf1326c42e3c7fa1637f
NandlalShah <shahnandlal49@gmail.com>
2024-09-06 15:50:37 +0530

    Search functionality completed

--- frontend/src/components/InputComponent.jsx
+++ frontend/src/components/InputComponent.jsx
@@ -1,22 +1,61 @@
-import React from 'react'
-import '../App.css';
+import "../App.css";
+import React from "react";
 import { Input, Button } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
+import * as ScrappedService from "../services/DataScrapService/DataScrapService";
+import { useDispatch } from "react-redux";
+import Swal from 'sweetalert2'
 function InputComponent({ inputText, setInputText }) {
+  const dispatch=useDispatch();
+  const handleSearch = () => {
+    const urlPattern = new RegExp('^(https?:\\/\\/)?'+
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + 
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + 
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + 
+      '(\\#[-a-z\\d_]*)?$', 'i'); 
+
+  // Validating the URL
+  if (!urlPattern.test(inputText)) {
+      Swal.fire({
+        icon: "info",
+        title:'Please Enter a valid Url',
+    });
+      return;
+  }
+    dispatch(ScrappedService.ScrapDatafromURL({url:inputText}))
+      .then((response) => {
+      console.log(response.data);
+      const message=response.data.message;
+      Swal.fire({
+        title: message,
+        icon: "success",
+    });
+      })
+      .catch((err) => {
+        let message = err.response.data.message;
+        Swal.fire({
+          title: message,
+          icon: "warning",
+          showCancelButton: true,
+      });
+        console.log({ err });
+      });
+  };
   return (
     <div className="search-container">
-    <Input
-      placeholder="Enter domain name"
-      suffix={<SearchOutlined />}
-      className="search-input"
-      value={inputText}
-      onChange={(e) => setInputText(e.target.value)}
-    />
-    <Button type="primary" className="fetch-btn">
-      Fetch & Save Details
-    </Button>
-  </div>
-  )
+      <Input
+        placeholder="Enter domain name"
+        suffix={<SearchOutlined />}
+        className="search-input"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      />
+      <Button type="primary" className="fetch-btn" onClick={handleSearch}>
+        Fetch & Save Details
+      </Button>
+    </div>
+  );
 }
 
 export default InputComponent;
--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {useLocation } from "react-router-dom";
+import * as ScrappedService from "../services/DataScrapService/DataScrapService";
+import { useDispatch } from "react-redux";
+import InputComponent from "../components/InputComponent";
 function Details() {
+  const dispatch = useDispatch();
   const location = useLocation();
+  const [Scrappeddata, setScrappeddata] = useState([]);
+  const [inputText, setInputText] = useState("");
   const { productId } = location.state || {};
-  console.log('productId', productId)
+  useEffect(() => {
+    dispatch(ScrappedService.getSingleScrappedData(productId))
+      .then((response) => {
+        setScrappeddata(response.data);
+      })
+      .catch((err) => {
+        console.log({ err });
+      });
+  }, [productId]);
+  // console.log('Scrappeddata', Scrappeddata)
   return (
-    <div>Details</div>
+    <div>
+       <InputComponent inputText={inputText} setInputText={setInputText}  />
+    </div>
   )
 }
 
